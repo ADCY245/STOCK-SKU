f251479b997592d5ac4485cfbd64b4091900348c
athulnairwork-star <athul.nair.work@gmail.com>
2025-12-24 18:11:12 +0530

    Fix Flask app to bind to 0.0.0.0 for Render deployment

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from routes.products import products_bp
@@ -13,4 +14,4 @@ app.register_blueprint(stock_bp, url_prefix='/api')
 app.register_blueprint(reports_bp, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
